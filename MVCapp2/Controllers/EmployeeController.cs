cd5440de8058f7a9ad611ad414470bfec4205a5e
AkmalKhan <AkmalKhan@DESKTOP-VOH7S7M>
2023-05-11 14:43:22 +0530

    Add project files.

---
+++ MVCapp2/Controllers/EmployeeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCapp2.Models;
+using MVCapp2.ViewModels;
+
+namespace MVCapp2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ViewResult Ddetails()
+        {
+            Employee employee1 = new Employee()
+            {
+                EmployeeId = 1,
+                Name = "Shahzar",
+                Gender="Male",
+                Department="IT",
+                Salary=50000,
+                AddressId=800
+            };
+            Employee employee2 = new Employee()
+            {
+                EmployeeId = 2,
+                Name = "Mazhar",
+                Gender = "Male",
+                Department = "IT",
+                Salary = 50000,
+                AddressId = 900
+            };
+            Employee employee3 = new Employee()
+            {
+                EmployeeId = 3,
+                Name = "Anzar",
+                Gender = "Male",
+                Department = "IT",
+                Salary = 50000,
+                AddressId = 1000
+            };
+            List<Employee> employeelist = new List<Employee>();
+            employeelist.Add(employee1);
+            employeelist.Add(employee2);
+            employeelist.Add(employee3);
+
+            Address address1 = new Address()
+            {
+                AddressId = 800,
+                Country="India",
+                State="U.P",
+                City="Meerut",
+                Pin="5008"
+            };
+            Address address2 = new Address()
+            {
+                AddressId = 900,
+                Country = "India",
+                State = "U.P",
+                City = "Meerut",
+                Pin = "6528"
+            };
+            Address address3 = new Address()
+            {
+                AddressId = 1000,
+                Country = "India",
+                State = "U.P",
+                City = "Meerut",
+                Pin = "7555"
+            };
+            List<Address> addresslist = new List<Address>();
+            addresslist.Add(address1);
+            addresslist.Add(address2);
+            addresslist.Add(address3);
+
+            EmployeesDetail viewModel = new EmployeesDetail()
+            {
+                Employee = employeelist,Address = addresslist,PageTitle="EmployeeDetailPage",PageHeader="EmployeesDetail"
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MVCapp2/ViewModels/EmployeeDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCapp2.Models;
+
+namespace MVCapp2.ViewModels
+{
+    public class EmployeesDetail
+    {
+        public List<Employee> Employee { get; set; }
+        public List<Address> Address { get; set; }
+        
+        public string PageTitle { get; set; }
+        public string PageHeader { get; set; }
+
+
+    }
+}
